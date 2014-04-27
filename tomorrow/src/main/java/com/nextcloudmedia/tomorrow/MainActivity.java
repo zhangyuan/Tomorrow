1f4fb326ebd84a20cdf298736d34a664881f2897
Yuan Cheung <evyuancheung@gmail.com>
2014-04-27 21:05:40 +0800

    Get Posts

--- tomorrow/src/main/java/com/nextcloudmedia/tomorrow/MainActivity.java
+++ tomorrow/src/main/java/com/nextcloudmedia/tomorrow/MainActivity.java
@@ -3,6 +3,7 @@ package com.nextcloudmedia.tomorrow;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,13 +12,29 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import com.avos.avoscloud.AVException;
+import com.avos.avoscloud.AVOSCloud;
+import com.avos.avoscloud.AVAnalytics;
+import com.avos.avoscloud.AVObject;
+import com.avos.avoscloud.AVQuery;
+import com.avos.avoscloud.FindCallback;
+
 
 public class MainActivity extends ActionBarActivity {
+    String app_id = "";
+    String app_key = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initAVOS();
+
         setContentView(R.layout.activity_main);
 
         if (savedInstanceState == null) {
@@ -27,6 +44,19 @@ public class MainActivity extends ActionBarActivity {
         }
     }
 
+    public void initAVOS() {
+        try {
+            InputStream inputStream = getAssets().open("app.properties");
+            Properties properties = new Properties();
+            properties.load(inputStream);
+            app_id = properties.getProperty("app_id");
+            app_key = properties.getProperty("app_key");
+        } catch (IOException e) {
+        }
+
+        AVOSCloud.initialize(this, app_id, app_key);
+        AVAnalytics.trackAppOpened(getIntent());
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -66,21 +96,34 @@ public class MainActivity extends ActionBarActivity {
         public void onActivityCreated(Bundle savedInstanceState) {
             super.onActivityCreated(savedInstanceState);
 
-            ListView itemsListView = (ListView) getActivity().findViewById(R.id.itemsListView);
+            final ListView itemsListView = (ListView) getActivity().findViewById(R.id.itemsListView);
 
-            final ArrayList<String> items = new ArrayList<String>();
+            AVQuery<AVObject> query = new AVQuery<AVObject>("Post");
+            query.setLimit(15);
+            query.findInBackground(new FindCallback<AVObject>() {
+                public void done(List<AVObject> avObjects, AVException e) {
+                    if (e == null) {
+                        Log.d("成功", "查询到" + avObjects.size() + " 条符合条件的数据");
 
-            String[] values = new String[] { "Monday", "Friday"};
 
-            for(int i = 0; i < values.length; i++){
-                items.add(values[i]);
-            }
+                        final ArrayList<String> items = new ArrayList<String>();
 
-            final ArrayAdapter<String> aa;
+                        for(int i = 0; i < avObjects.size(); i++){
+                            items.add(avObjects.get(i).getString("title"));
+                        }
 
-            aa = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, items);
+                        final ArrayAdapter<String> aa;
 
-            itemsListView.setAdapter(aa);
+                        aa = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, items);
+
+                        itemsListView.setAdapter(aa);
+
+
+                    } else {
+                        Log.d("失败", "查询错误: " + e.getMessage());
+                    }
+                }
+            });
         }
     }
 
